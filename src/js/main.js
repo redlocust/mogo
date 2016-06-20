1e4684bc65e2d00b00cf93acf703448f179ad57f
redlocust <redlocust1@gmail.com>
2016-06-20 17:15:51 +0300

    add animate.css and jquery waypoint for animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@
  * Third party
  */
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/waypoints/lib/jquery.waypoints.js
+//= ../../bower_components/jquery.counterup/jquery.counterup.js
+
 
 /*
  * Custom
---
+++ src/js/partials/app.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+  $('.counter').counterUp({
+    delay: 100,
+    time: 3000
+  });
+
+});
+
+$('#waypoint').waypoint(function() {
+  $('').addClass('animated').addClass('bounce').addClass('infinite');
+},{
+  triggerOnce: true,
+  offset: '90%'
+});
