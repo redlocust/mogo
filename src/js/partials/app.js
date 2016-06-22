456f9a84f8b55945dfb1d95eb0eaafd7c50a8700
redlocust <redlocust1@gmail.com>
2016-06-22 17:07:50 +0300

    add features and service2 sections

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,14 +1,16 @@
-jQuery(document).ready(function($) {
+$(function($) {
+
   $('.counter').counterUp({
     delay: 100,
     time: 3000
   });
 
-});
 
-$('#waypoint').waypoint(function() {
-  $('').addClass('animated').addClass('bounce').addClass('infinite');
-},{
-  triggerOnce: true,
-  offset: '90%'
+  $('#waypoint').waypoint(function() {
+    $('').addClass('animated').addClass('bounce').addClass('infinite');
+  },{
+    triggerOnce: true,
+    offset: '90%'
+  });
+
 });
