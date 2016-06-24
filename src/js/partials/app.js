80fc28721a5e42169a1041f2529c1ebe8f5020a9
redlocust <redlocust1@gmail.com>
2016-06-24 13:04:29 +0300

    add accordion to service section

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -13,4 +13,10 @@ $(function($) {
     offset: '90%'
   });
 
+  $('.js-accordion-trigger').bind('click', function(e){
+    jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
+
 });
