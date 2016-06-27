f38200cb9372b5e9fcc062f57a90cd54b6e46e65
redlocust <redlocust1@gmail.com>
2016-06-28 00:09:41 +0300

    ...

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,25 +1,23 @@
-$(function($) {
+$(function ($) {
 
-  $('.counter').counterUp({
-    delay: 100,
-    time: 3000
-  });
-
-
-  $('#waypoint').waypoint(function() {
-    $('').addClass('animated').addClass('bounce').addClass('infinite');
-  },{
-    triggerOnce: true,
-    offset: '90%'
-  });
-
-  $('.js-accordion-trigger').bind('click', function(e){
-    jQuery(this).parent().siblings().find('.submenu').slideUp('700');
-    jQuery(this).parent().siblings().removeClass('is-expanded');
-    jQuery(this).parent().find('.submenu').slideToggle('700');
-    jQuery(this).parent().toggleClass('is-expanded');
-    e.preventDefault();
-  });
+    $('.counter').counterUp({
+        delay: 100,
+        time: 3000
+    });
 
 
+    $('#waypoint').waypoint(function () {
+        $('').addClass('animated').addClass('bounce').addClass('infinite');
+    }, {
+        triggerOnce: true,
+        offset: '90%'
+    });
+
+    $('.js-accordion-trigger').bind('click', function (e) {
+        jQuery(this).parent().siblings().find('.submenu').slideUp('700');
+        jQuery(this).parent().siblings().removeClass('is-expanded');
+        jQuery(this).parent().find('.submenu').slideToggle('700');
+        jQuery(this).parent().toggleClass('is-expanded');
+        e.preventDefault();
+    });
 });
