380cee0293f35828b0bc3cb6065708835f77a5f5
redlocust <redlocust1@gmail.com>
2016-06-28 16:12:26 +0300

    complete accordion with scroll

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
  */
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/waypoints/lib/jquery.waypoints.js
+//= ../../bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.js
 //= ../../bower_components/jquery.counterup/jquery.counterup.js
 
 
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,23 +1,28 @@
-$(function ($) {
+$(function () {
 
-    $('.counter').counterUp({
-        delay: 100,
-        time: 3000
-    });
+  //open first panel of accordeon
+  $('.submenu:first').show();
+  $('.submenu').perfectScrollbar();
 
 
-    $('#waypoint').waypoint(function () {
-        $('').addClass('animated').addClass('bounce').addClass('infinite');
-    }, {
-        triggerOnce: true,
-        offset: '90%'
-    });
+  //roll numbers
+  $('.counter').counterUp({
+    delay: 100,
+    time: 3000
+  });
 
-    $('.js-accordion-trigger').bind('click', function (e) {
-        jQuery(this).parent().siblings().find('.submenu').slideUp('700');
-        jQuery(this).parent().siblings().removeClass('is-expanded');
-        jQuery(this).parent().find('.submenu').slideToggle('700');
-        jQuery(this).parent().toggleClass('is-expanded');
-        e.preventDefault();
-    });
+  $('#waypoint').waypoint(function () {
+    $('').addClass('animated').addClass('bounce').addClass('infinite');
+  }, {
+    triggerOnce: true,
+    offset: '90%'
+  });
+
+  $('.js-accordion-trigger').bind('click', function (e) {
+    jQuery(this).parent().siblings().find('.submenu').slideUp('600');
+    jQuery(this).parent().siblings().removeClass('is-expanded');
+    jQuery(this).parent().find('.submenu').slideToggle('600');
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
 });
